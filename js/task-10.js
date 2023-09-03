5ffb0ea37fa40acfbf008d6dfaf4574b0ebfa8e7
anutkaavslchnk <anutochkaaaaa14@gmail.com>
2023-09-03 21:17:29 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,8 +3,25 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
-const divBoxes=document.querySelector("#boxes");
-function createBoxes(amount){
-  const newDiv=document.createElement("div");
-divBoxes.append(newDiv);
+const divBoxes = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  let width = 30;
+  let height = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const newDiv = document.createElement("div");
+
+    newDiv.style.width = `${width}px`;
+    newDiv.style.height = `${height}px`;
+
+    newDiv.style.backgroundColor = getRandomHexColor();
+    divBoxes.appendChild(newDiv);
+
+    width += 10;
+    height += 10;
+  }
+}
+function destroyBoxes() {
+  divBoxes.innerHTML = "";
 }
